21f87025774c50deb28e61d2890b21c182278531
Nguyen Hoang Hai <hainh2k3@live.com>
2015-12-17 14:57:55 +0700

    updateed

---
+++ DemoChatServer/DemoChatServer/Server.js
@@ -0,0 +1,11 @@
+﻿
+var http = require('http');
+
+
+function serverListener(request, response) {
+	response.writeHead(200, { 'Content-Type': 'text/plaint' });
+	response.write("ok server started!");
+	response.end();
+}
+
+http.createServer(serverListener).listen(80);
