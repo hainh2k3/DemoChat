dda66ef28b685adab5ab430029b2512248bfee98
Nguyen Hoang Hai <hainh2k3@live.com>
2015-12-17 16:28:07 +0700

    ok

--- DemoChatServer/DemoChatServer/Server.js
+++ DemoChatServer/DemoChatServer/Server.js
@@ -1,11 +1,24 @@
 ﻿
 var http = require('http');
+var io = require('socket.io').listen(8000);
+var fs = require('fs');
+
+io.sockets.on('connection', function (socket) {
+	socket.on('tn_den_server', function (data) {
+		var _dt = new Date();
+		var _tg = _dt.getHours() + ':' + _dt.getMinutes() + ':' + _dt.getSeconds();
+		io.sockets.emit('tn_den_client', { message: data['message'] , thoigian : _tg });
+	});
+});
+
 
 
 function serverListener(request, response) {
-	response.writeHead(200, { 'Content-Type': 'text/plaint' });
-	response.write("ok server started!");
-	response.end();
+	fs.readFile('www/index.html', { 'Encoding': 'utf-8' }, function (err, data) {
+		response.writeHead(200, { 'Content-Type': 'text/html' });
+		response.write(data);
+		response.end();
+	});
 }
 
 http.createServer(serverListener).listen(80);
